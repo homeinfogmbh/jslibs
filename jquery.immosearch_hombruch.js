c918ef89e8bc4219ce6a0ba5a4f5d4577f5312dd
Nick <nik@homeinfo.de>
2016-07-28 15:46:20 +0200

    Change the grid system.

--- jquery.immosearch_hombruch.js
+++ jquery.immosearch_hombruch.js
@@ -1389,7 +1389,14 @@ function homeinfo_immosearch_details(object_id, cid, container, preloadeGif, imm
 		        immoDetailElement += '</div>';
 
 						immoDetailElement += '<div class="col-md-12 col-sm-12 col-xs-12">';
-							immoDetailElement += '<div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, </div>';
+
+							immoDetailElement += '<div class="col-md-6">';
+								immoDetailElement += '<h4><strong>COLUMN 1</strong></h4>';
+							immoDetailElement += '</div>';
+							immoDetailElement += '<div class="col-md-6">';
+								immoDetailElement += '<h4><strong>COLUMN 2</strong></h4>';
+							immoDetailElement += '</div>';
+
 						immoDetailElement += '</div>';
 
 		        immoDetailElement += '<div class="col-md-12 col-sm-12 col-xs-12">';
