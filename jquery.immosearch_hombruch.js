bf34469a15df8349a20a50feafa3f7421406e82c
Nick <nik@homeinfo.de>
2016-08-01 15:25:30 +0200

    Change the position of the elements

--- jquery.immosearch_hombruch.js
+++ jquery.immosearch_hombruch.js
@@ -1656,11 +1656,10 @@ function homeinfo_immosearch_details(object_id, cid, container, preloadeGif, imm
 		        immoDetailElement += '<div class="col-md-12 col-sm-12 col-xs-12">';
 
 		          immoDetailElement += '<div class="col-md-6">';
-
+								immoDetailElement += 'test 1';
 		          immoDetailElement += '</div>';
 		          immoDetailElement += '<div class="col-md-6">';
-
-
+								immoDetailElement += 'test 1';
 		          immoDetailElement += '</div>';
 		        immoDetailElement += '</div>';
 
