f11efea79e265a2e1ff859e9a1d33112e0b0962b
Richard Neumann <r.neumann@homeinfo.de>
2017-02-28 16:07:14 +0100

    Updated copyright year

--- homeinfo.js
+++ homeinfo.js
@@ -1,7 +1,7 @@
 /*
   HOMEINFO general-purpose Javascript library
 
-  (C) 2015 HOMEINFO - Digitale Informationssysteme GmbH
+  (C) 2015-2017 HOMEINFO - Digitale Informationssysteme GmbH
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
