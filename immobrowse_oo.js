48611863f7fe542cd10313a040f975b4e5002854
Richard Neumann <r.neumann@homeinfo.de>
2017-03-03 11:35:17 +0100

    dito

--- immobrowse_oo.js
+++ immobrowse_oo.js
@@ -1090,7 +1090,7 @@ immobrowse.List = function (cid, sorting, exposeBaseUrl) {
     }
 
     if (xmlHttp) {
-      xmlHttp.open('GET', 'https://tls.homeinfo.de/immobrowse/list/' + this.cid, true);
+      xmlHttp.open('GET', 'https://tls.homeinfo.de/immobrowse/list/' + this.cid, false);
       xmlHttp.send(null);
       immobrowse.logger.debug('Got XMLHTTP response:');
       immobrowse.logger.debug(xmlHttp.responseText);
@@ -1130,7 +1130,7 @@ immobrowse.Expose = function (cid, objektnr_extern, listUrl) {
     }
 
     if (xmlHttp) {
-      xmlHttp.open('GET', 'https://tls.homeinfo.de/immobrowse/real_estate/' + this_.objektnr_extern + '?customer=' + this_.cid, true);
+      xmlHttp.open('GET', 'https://tls.homeinfo.de/immobrowse/real_estate/' + this_.objektnr_extern + '?customer=' + this_.cid, false);
       xmlHttp.send(null);
       immobrowse.logger.debug('Got XMLHTTP response:');
       immobrowse.logger.debug(xmlHttp.responseText);
