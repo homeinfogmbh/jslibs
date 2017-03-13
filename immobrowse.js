489b8cb22342ae1b9ac928ebf153ea91246a88f0
Richard Neumann <r.neumann@homeinfo.de>
2017-03-13 14:35:26 +0100

    Setting source to empty value

--- immobrowse.js
+++ immobrowse.js
@@ -1667,7 +1667,7 @@ immobrowse.List = function (cid, realEstates) {
     }
 
     if (elements.titleImage != null) {
-      elements.titleImage.attr('src', null);
+      elements.titleImage.attr('src', '');
     }
   }
 
