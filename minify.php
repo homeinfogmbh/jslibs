4596894980caaab39a023869cce830fc58111916
Richard Neumann <r.neumann@homeinfo.de>
2017-03-14 16:55:21 +0100

    Added correct content type

--- minify.php
+++ minify.php
@@ -5,6 +5,7 @@
         $path = '/srv/http/de/homeinfo/jslibs/' . $script;
 
         if (file_exists($path)) {
+            header('Content-Type: application/javascript');
             system('/usr/bin/uglifyjs ' . $path . ' -m');
         } else {
             print('Invalid script: ' . $script . '.');
